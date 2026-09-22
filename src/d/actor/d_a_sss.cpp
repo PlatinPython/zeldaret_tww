03c260037e0f8920409dd47f5cfa8f0546438288
PlatinPython <robert.alexander.kaempf@gmail.com>
2026-09-23 00:04:24 +0300

    d_a_sss 87%

--- src/d/actor/d_a_sss.cpp
+++ src/d/actor/d_a_sss.cpp
@@ -5,6 +5,7 @@
 
 #include "d/dolzel_rel.h" // IWYU pragma: keep
 #include "d/actor/d_a_sss.h"
+#include "d/actor/d_a_player_main.h"
 #include "d/d_cc_d.h"
 #include "d/d_s_play.h"
 #include "res/Object/Sss.h"
@@ -62,7 +63,7 @@ void hand_mtx_set(sss_class* i_this) {
     mDoMtx_XrotM(*calc_mtx, REG12_S(1) + -0x4000);
     MtxScale(0.5, REG0_F(0) + 0.2f, 0.5, true);
     MtxTrans(0, REG12_F(3) + -130, 0, true);
-    PSMTXCopy(*calc_mtx, i_this->field_0x2B4->getModel()->getBaseTRMtx());
+    MTXCopy(*calc_mtx, i_this->field_0x2B4->getModel()->getBaseTRMtx());
 }
 
 /* 00000444-000004CC       .text control3__FP9sss_class */
@@ -350,8 +351,381 @@ void cut_control2(sss_class* i_this) {
 }
 
 /* 000014F4-00002614       .text hand_move__FP9sss_class */
-void hand_move(sss_class*) {
+void hand_move(sss_class* i_this) {
     /* Nonmatching */
+    f32 speedRatio;
+    f32 fVar1;
+    f32 target;
+    f32 maxSpeed;
+    float fVar2;
+    float fVar3;
+    float fVar4;
+    bool bVar5;
+    bool bVar6;
+    bool bVar7;
+    daPy_py_c *apdVar8 [1];
+    daPy_lk_c *pdVar9;
+    uint uVar10;
+    bool bVar16;
+    short sVar15;
+    u8 *puVar12;
+    u8 *puVar13;
+    cXyz *pcVar14;
+    sss_s *psVar18;
+    cXyz *pcVar19;
+    dCcD_GObjInf* objInf;
+    int iVar20;
+    int iVar21;
+    cBgS_GrpPassChk *pcVar21;
+    int iVar22;
+    double dVar23;
+    float fVar25;
+    float fVar26;
+    cXyz local_1d0;
+    cXyz local_1c4;
+    cXyz local_1b8;
+    cXyz local_1ac;
+    cXyz local_1a0;
+    cXyz local_194;
+    cXyz local_188;
+    cXyz local_17c;
+    cXyz cStack_170;
+    cXyz local_164;
+    CcAtInfo local_158;
+    cBgS_GndChk local_13c;
+    cBgS_GrpPassChk local_fc;
+    cBgS_GrpPassChk local_f0;
+    f64 local_e8;
+
+
+    pdVar9 = (daPy_lk_c*) dComIfGp_getLinkPlayer();
+    apdVar8[0] = (daPy_py_c*) dComIfGp_getPlayer(0);
+    local_13c = cBgS_GndChk();
+    pcVar21 = &local_fc;
+    
+    // TODO
+
+    if (pcVar21) {
+        local_13c.SetGrpPassChk(&local_f0);
+    }
+    bVar6 = false;
+    bVar7 = false;
+    speedRatio = 0.1;
+    fVar25 = 8;
+    fVar1 = 0;
+    target = 30;
+    maxSpeed = 1;
+    local_13c.SetPolyPassChk((cBgS_PolyPassChk*) pcVar21);
+    dVar23 = fopAcM_searchActorDistance(i_this, dComIfGp_getPlayer(0));
+    fVar2 = 5;
+    uVar10 = i_this->field_0x2B9;
+    if (uVar10 == 0xFF) {
+        fVar25 = 1000;
+    } else {
+
+        // TODO
+
+        fVar25 = uVar10 * 10;
+    }
+    mDoMtx_YrotS(*calc_mtx, i_this->current.angle.y);
+    mDoMtx_XrotM(*calc_mtx, i_this->current.angle.x);
+    bVar5 = false;
+    fVar4 = speedRatio;
+    fVar3 = fVar1;
+    switch (i_this->field_0x2C0) {
+        case 0:
+            local_17c = i_this->current.pos;
+            target = 0;
+            maxSpeed = 0.5;
+            fVar3 = -20;
+            if (!i_this->field_0x2C2[0]) {
+                if (!i_this->field_0x2BA) {
+                    if (dVar23 < fVar25) {
+                        i_this->field_0x2C0 = 1;
+                        i_this->field_0x2C2[0] = 30;
+                        hand_open(i_this);
+                        bVar5 = true;
+                    }
+                } else {
+                    bVar16 = dComIfGs_isSwitch(i_this->field_0x2BA, fopAcM_GetRoomNo(i_this));
+                    if (bVar16 || REG0_S(0)) {
+                        i_this->field_0x2C0 = 1;
+                        i_this->field_0x2C2[0] = 30;
+                        hand_open(i_this);
+                        bVar5 = true;
+                    }
+                }
+            }
+            if (std::abs(i_this->field_0x2C8.y - i_this->current.pos.y) < 5 && bVar5) {
+                if (i_this->field_0x2B8 == 1) {
+                    fopAcM_seStart(i_this, JA_SE_OBJ_SVINE_OUT_WATER, 0);
+                } else {
+                    fopAcM_seStart(i_this, JA_SE_OBJ_SVINE_OUT, 0);
+                }
+            }
+            break;
+        case 1:
+            local_164.x = (REG0_F(13) + 50) * cM_ssin(i_this->field_0x2BC * 600);
+            local_164.y = 250;
+            local_164.z = (REG0_F(13) + 50) * cM_ssin(i_this->field_0x2BC * 700);
+            MtxPosition(&local_164, &cStack_170);
+            local_1ac = i_this->current.pos + cStack_170;
+            local_17c = local_1ac;
+            if (i_this->field_0x2C2[0] == 0 && dVar23 < (REG0_F(14) + 300)) {
+                i_this->field_0x2C0 = 2;
+            }
+            if ((fVar25 + 100) < dVar23) {
+                i_this->field_0x2C0 = 0;
+            }
+            break;
+        case 2:
+            fVar25 = 15;
+            fVar1 = 10;
+            bVar6 = true;
+            local_17c.x = i_this->current.pos.x;
+            local_17c.z = i_this->current.pos.z;
+            local_17c.y = i_this->current.pos.y + 70;
+            if ((REG0_F(14) + 450) < dVar23) {
+                i_this->field_0x2C0 = 1;
+                i_this->speedF = 0;
+            }
+            local_1b8 = local_17c - i_this->field_0x2C8;
+            local_164 = local_1b8;
+            fVar25 = VECSquareMag(&local_164);
+            fVar25 = std::sqrtf(fVar25);
+            fVar4 = 0.5;
+            if (fVar25 >= 20 || apdVar8[0] != pdVar9) break;
+            i_this->field_0x2C0 = 3;
+            hand_close(i_this);
+            fopAcM_seStart(i_this, JA_SE_OBJ_SVINE_GRASP, 0);
+        case 3:
+            apdVar8[0]->onNoResetFlg1(daPy_lk_c::daPyFlg1_LAST_COMBO_WAIT);
+            apdVar8[0]->setFace(daPy_lk_c::daPyFace_TIYAYA);
+            if (dComIfGs_getMagic()) {
+                g_dComIfG_gameInfo.play.field_0x4965 |= 1;
+                fopAcM_seStart(i_this, JA_SE_OBJ_ATK_VINE_MP_SUCK, 0);
+                if ((i_this->field_0x2BC & 0x1F) == 0) {
+                    dComIfGp_setItemMagicCount(-1);
+                }
+            }
+            if ((REG6_F(2) + 400) < dVar23) {
+                dVar23 = (REG6_F(0) + 0.1) * (dVar23 - REG6_F(2) + 400);
+                if ((REG6_F(1) + 100) < dVar23) {
+                    dVar23 = REG6_F(1) + 100;
+                }
+                sVar15 = fopAcM_searchActorAngleY(i_this, dComIfGp_getPlayer(0));
+                apdVar8[0]->setOutPower(dVar23, sVar15 - 0x8000, 0);
+            }
+            local_17c.x = apdVar8[0]->current.pos.x;
+            local_17c.z = apdVar8[0]->current.pos.z;
+            local_17c.y = apdVar8[0]->current.pos.y + 70;
+            fVar25 = 200;
+            speedRatio = 1;
+            i_this->field_0x2F4 = REG0_F(11) + 10;
+            fVar4 = 5;
+            if (dVar23 > 800) {
+                i_this->field_0x2C0 = 1;
+                i_this->speedF = 30;
+                hand_open(i_this);
+            }
+            break;
+        case 5:
+            fVar2 = 50;
+            i_this->field_0x2F4 = 50;
+            bVar7 = true;
+            target = 25;
+            maxSpeed = 1;
+            VECAdd(&i_this->field_0x2C8, &i_this->speed, &i_this->field_0x2C8);
+            i_this->speed.y -= 3;
+            i_this->field_0x2C2[2] = 5;
+            local_13c.m_pos.z = i_this->field_0x2C8.z;
+            local_13c.m_pos.y = i_this->field_0x2C8.y + 200;
+            local_13c.m_pos.x = i_this->field_0x2C8.x;
+            fVar25 = g_dComIfG_gameInfo.play.mBgS.GroundCross(&local_13c);
+            i_this->field_0x2F8 = fVar25;
+            fVar25 = i_this->field_0x2F8;
+            if ((fVar25 == -1e9) || (i_this->field_0x2C8.y <= fVar25 + 10)) {
+                i_this->field_0x2C8.y = fVar25 + 10;
+                i_this->field_0x2C2[0] = 100;
+                i_this->field_0x2C0 = 6;
+            }
+            break;
+        case 6:
+            bVar7 = true;
+            fVar2 = 0;
+            i_this->field_0x2C2[2] = 10;
+            if (i_this->field_0x2C2[0] < 40) {
+                i_this->field_0x2C8.y -= 2;
+                target = fVar2;
+            }
+            maxSpeed = 1;
+            if (i_this->field_0x2C2[0] == 0) {
+                i_this->field_0x2C8 = i_this->current.pos;
+                i_this->field_0x2C0 = 0;
+                i_this->field_0x2E8 = 0;
+                i_this->field_0x2FC = 0;
+            }
+    }
+    cLib_addCalc2(&i_this->field_0x2E8, target, 0.5, maxSpeed);
+    cLib_addCalc2(&i_this->field_0x2F0, fVar1, 1, 0.2);
+    cLib_addCalc2(&i_this->field_0x2F4, fVar2, 1, 1.5);
+    if (bVar7) {
+        cut_control1(i_this);
+        cut_control2(i_this);
+        psVar18 = i_this->field_0x490;
+        pcVar19 = i_this->field_0x454.getPos(0);
+        puVar12 = i_this->field_0x454.getSize(0);
+        for (iVar22 = 5; iVar22 != 0; iVar22--) {
+            *pcVar19 = psVar18->field_0x00;
+            local_e8 = psVar18->field_0x18;
+            *puVar12 = psVar18->field_0x18;
+            psVar18 += 1;
+            pcVar19 += 1;
+            puVar12 += 1;
+        }
+        cLib_addCalc0(&i_this->field_0x2FC, 1, REG0_F(1) + 1);
+    } else {
+        cLib_addCalc2(&i_this->speedF, fVar26, 1, fVar4);
+        if (i_this->field_0xA08 > 1 && i_this->field_0x2C0 != 3) {
+            mDoMtx_YrotS(*calc_mtx, i_this->field_0xA0C);
+            local_164.x = 0;
+            local_164.y = REG6_F(9) + 100;
+            local_164.z = i_this->field_0xA08;
+            MtxPosition(&local_164, &cStack_170);
+            local_1c4 = i_this->current.pos + cStack_170;
+            local_17c = local_1c4;
+            speedRatio = 0.1;
+            i_this->speedF = i_this->field_0xA08 * 0.2;
+            if (REG6_F(8) + 30 < i_this->speedF) {
+                i_this->speedF = REG6_F(8) + 30;
+            }
+        }
+        cLib_addCalc0(&i_this->field_0xA08, 1, REG6_F(7) + 5);
+        cLib_addCalc2(&i_this->field_0x2C8.x, local_17c.x, speedRatio, i_this->speedF);
+        cLib_addCalc2(&i_this->field_0x2C8.y, local_17c.y, speedRatio, i_this->speedF);
+        cLib_addCalc2(&i_this->field_0x2C8.z, local_17c.z, speedRatio, i_this->speedF);
+        cLib_addCalc2(&i_this->current.pos.y, i_this->home.pos.y + fVar3, 0.5, 0.5);
+        if (bVar6 && i_this->current.angle.x == 0) {
+            sVar15 = fopAcM_searchActorAngleY(i_this, dComIfGp_getPlayer(0));
+            cLib_addCalcAngleS2(&i_this->current.angle.y, sVar15, 16, 2048);
+        }
+        control1(i_this);
+        control2(i_this);
+    }
+    control3(i_this);
+    i_this->field_0x2B4->play(NULL, 0, 0);
+    psVar18 = i_this->field_0x33C;
+    pcVar19 = i_this->field_0x300.getPos(0);
+    puVar13 = i_this->field_0x300.getSize(0);
+    for (iVar22 = 10; iVar22 != 0; iVar22--) {
+        *pcVar19 = psVar18->field_0x00;
+        local_e8 = psVar18->field_0x18;
+        *puVar13 = psVar18->field_0x18;
+        psVar18 += 1;
+        pcVar19 += 1;
+        puVar13 += 1;
+    }
+    pcVar19 = i_this->field_0x300.getPos(0);
+    i_this->eyePos = pcVar19[5];
+    i_this->attention_info.position = i_this->eyePos;
+    i_this->field_0x51C.Move();
+    if (bVar7) {
+        i_this->field_0x8DC.SetC(non_pos);
+    } else {
+        i_this->field_0x8DC.SetC(i_this->eyePos);
+    }
+    uVar10 = 0;
+    dComIfG_Ccsp()->Set(&i_this->field_0x8DC);
+    iVar20 = 0;
+    iVar22 = 0;
+    for (iVar21 = 0; iVar21 < 3; iVar21++) {
+        pcVar14 = pcVar19 + ((i_this->field_0x2BC & 3) + iVar22) % 10;
+        local_188 = *pcVar14;
+        if (bVar7) {
+            i_this->field_0x558[iVar21].SetC(non_pos);
+        } else {
+            i_this->field_0x558[iVar21].SetC(local_188);
+        }
+        if (i_this->field_0x2C0 == 3) {
+            i_this->field_0x558[iVar21].OffCoSPrmBit(1);
+        } else {
+            i_this->field_0x558[iVar21].OnCoSPrmBit(1);
+        }
+        dComIfG_Ccsp()->Set(i_this->field_0x558[0].GetCoHitObj());
+        iVar20 += 300;
+        iVar22 += 2;
+    }
+    iVar22 = 0;
+    for (iVar20 = 0; iVar20 < 3; iVar20++) {
+        iVar21 = i_this->field_0x558[iVar20].ChkAtHit();
+        if (iVar21 != 0) {
+            uVar10 = iVar20 + 1 & 0xFF;
+            break;
+        }
+        iVar22 += 300;
+    }
+    if ((uVar10 != 0 || i_this->field_0x8DC.ChkTgHit() != 0) && i_this->field_0x2C2[2] == 0) {
+        local_158.pParticlePos = NULL;
+        i_this->field_0x2C2[2] = 20;
+        if (uVar10 == 0) {
+            local_158.mpObj = i_this->field_0x8DC.GetTgHitObj();
+            local_158.pParticlePos = i_this->field_0x8DC.GetTgHitPosP();
+            at_power_check(&local_158);
+            if (local_158.mResultingAttackType == 8) {
+                i_this->field_0xA08 = REG6_F(6) + 300;
+                sVar15 = fopAcM_searchActorAngleY(i_this, dComIfGp_getPlayer(0));
+                i_this->field_0xA0C = sVar15 - 0x8000;
+
+                // TODO
+
+                return;
+            }
+        } else {
+
+            // TODO
+
+            local_158.mpObj = objInf->GetTgHitObj();
+            local_158.pParticlePos = objInf->GetTgHitPosP();
+        }
+        fopAcM_seStart(i_this, JA_SE_LK_LAST_HIT, 0);
+        fopAcM_seStart(i_this, JA_SE_OBJ_SVINE_CRASH, 0);
+        i_this->field_0x2C0 = 5;
+        fVar26 = cM_rndFX(10);
+        i_this->speed.x = fVar26;
+        fVar26 = cM_rndFX(10);
+        i_this->speed.y = fVar26 + 30;
+        fVar26 = cM_rndFX(10);
+        i_this->speed.z = fVar26;
+        local_194.x = 0.3;
+        local_194.y = 0.3;
+        local_194.z = 0.3;
+        dComIfGp_particle_set(0x16, &i_this->eyePos, NULL, &local_194);
+        i_this->field_0xA18 = true;
+        psVar18 = i_this->field_0x33C;
+        pcVar19 = &i_this->field_0x490[0].field_0x00;
+        for (iVar22 = 0; iVar22 < 5; iVar22++) {
+            *pcVar19 = psVar18->field_0x00;
+            pcVar19[2].x = psVar18->field_0x18;
+            if (iVar22 == 4) {
+
+                // TODO
+
+                local_1a0 = local_1d0;
+                fVar26 = VECSquareMag(&local_1a0);
+                fVar26 = std::sqrtf(fVar26);
+                i_this->field_0x2FC = (REG0_F(2) + 1.5) * fVar26;
+            }
+            psVar18 += 1;
+            
+            // TODO
+
+        }
+        hand_open(i_this);
+    }
+    
+    // TODO
+
+    return;
 }
 
 /* 00002614-00002618       .text hand_main__FP9sss_class */
